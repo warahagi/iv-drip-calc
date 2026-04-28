c5183545cc6ca43b833b704e2830397ba298ca76
warahagi <warahagi@users.noreply.github.com>
2026-04-28 20:00:21 +0900

    iPhone通知不具合の修正とスリープ防止機能の追加

--- sw.js
+++ sw.js
@@ -23,3 +23,22 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+// 通知がクリックされた時の処理
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
+});
